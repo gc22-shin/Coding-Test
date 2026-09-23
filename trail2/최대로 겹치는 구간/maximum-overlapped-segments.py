a98fe2313f26a0159e5e143d624a0242b832460f
신규철 <43338903+gc22-shin@users.noreply.github.com>
2026-09-23 17:45:37 +0900

    [Passed] 최대로 겹치는 구간 (46ms, 16MB)

--- trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -24,11 +24,5 @@ total = [0 for _ in range(end - start+ 1)] #겹치는 구간 계산용
 for a, b in segments_changed: # 각 선분 뽑기
     for i in range(a, b):  # 각 선분 시작점기준 계산 -> b까지만 계산
         total[i] += 1
-
-max= 0
-
-for i in total:
-    if i > max:
-        max = i
         
-print(max)
+print(max(total))
